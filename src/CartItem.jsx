842ed6744d34b27b31766eeea195efaf1d05728e
aymanoski <mohamedgt1234@gmail.com>
2024-07-28 13:21:28 -0400

    commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -15,8 +15,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
+    onContinueShopping(e);
   };
   const handleCheckoutShopping = (e) => {
   alert('Functionality to be added for future reference');
@@ -30,7 +29,8 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    item.quantity--;
+    if(item.quantity!=0){
+    item.quantity--;}
     dispatch(updateQuantity(item));
 
    
@@ -70,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1"onClick={(e)=>handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
